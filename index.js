22eb735de755fb922f0333fb38e308fe812ad815
nikelborm <r0075301net.r@yandex.ru>
2020-12-12 20:33:20 +0300

    asd

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const { exec } = require("child_process");
 const WebSocket = require("ws");
 const timerrrr = setInterval( () => {
-	
 	exec( "cat /sys/class/thermal/thermal_zone0/temp", ( error, stdout, stderr ) => {
-	    if ( error ) {
-	        console.log( `error: ${ error.message }` );
-	        return;
-	    }
-	    console.log( parseInt( stdout.split( "\n" )[ 0 ], 10 ) / 1000 );
-	} );	
+		if ( error ) {
+			console.log( `error: ${ error.message }` );
+			return;
+		}
+		console.log( parseInt( stdout.split( "\n" )[ 0 ], 10 ) / 1000 );
+	} );
 }, 500 );
