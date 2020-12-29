07ce2d43ad9c6beea7499276ac4874aac4ab0880
nikelborm <kolya007.klass@gmail.com>
2020-12-30 02:38:47 +0300

    asd

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps : [{
+    name: 'farm',
+    script: 'index.js',
+    watch: '.',
+    ignore_watch: ["node_modules"]
+  }],
+
+  deploy : {
+    production : {
+      user : 'ubuntu',
+      host : 'ubuntu',
+      ref  : 'origin/main',
+      repo : 'https://github.com/nikelborm/farm',
+      'pre-deploy-local': 'yarn',
+    }
+  }
+};
